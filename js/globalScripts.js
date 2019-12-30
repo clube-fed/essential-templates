c2c13f235583d7b560585e86e924405698fce446
ce-joe <42188640+ce-joe@users.noreply.github.com>
2019-12-30 10:19:47 -0500

    Update globalScripts.js

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -196,13 +196,25 @@ $j('div:not(.left-menu) > .navbar-nav > li.dropdown:nth-last-child(-n+3) > ul.dr
 
 // Copy last top nav item link to navbar for flex purposes
 $j('header.opt2').each(function(){
+    var last_nav_item_text = $j.trim($j('.navbar-nav > li:last-child > .nav-link > .rootLevelSpan',this).text());
     $j('.navbar-nav > li:last-child > .nav-link', this).clone().removeClass('.nav-link').addClass('abut-secondary log-link').appendTo('.navbar');
-    $j('a.log-link', this).text('Login');
+    if (last_nav_item_text == 'Login') {
+        $j('a.log-link', this).text('Login');
+    }
+    else {
+        $j('a.log-link', this).text('Logout');
+    }
     $j('a.log-link', this).wrap( "<div class='nav-log-wrap'></div>" );
 });
 $j('header.opt4').each(function(){
+    var last_nav_item_text = $j.trim($j('.navbar-nav > li:last-child > .nav-link > .rootLevelSpan',this).text());
     $j('.navbar-nav > li:last-child > .nav-link', this).clone().removeClass('.nav-link').addClass('log-link').appendTo('.navbar');    
-    $j('a.log-link', this).text('Login');
+    if (last_nav_item_text == 'Login') {
+        $j('a.log-link', this).text('Login');
+    }
+    else {
+        $j('a.log-link', this).text('Logout');
+    }
     $j('a.log-link', this).wrap( "<div class='nav-log-wrap'></div>" );
     $j('a.log-link', this).prepend( "<span class='nc-icon-outline ui-1_lock'></span>" );
 });
