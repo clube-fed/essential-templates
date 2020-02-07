a906ee7770435ab5b62dcfd2fe52207ad68bd75d
lofisoul <jason.nolfi@gmail.com>
2020-02-07 08:38:38 -0600

    Bug with TrumptheNav function

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -624,23 +624,23 @@ if ($j('.navbar').hasClass('navbar-expand-lg')){var mobileNavMaxBreak = 991;}
 if ($j('.navbar').hasClass('navbar-expand-md')){var mobileNavMaxBreak = 767;}
     
 	if ($j(window).width() > mobileNavMaxBreak) {
-		$j('body.page').addClass('desktopNav');
-		$j('body.page').removeClass('burgerNav');
+		$j('body').addClass('desktopNav');
+		$j('body').removeClass('burgerNav');
 		$j('header .navbar-toggle').attr('tabindex','-1').removeAttr('aria-label');
 		$j('.navbar-header .sr-only').text('');
 		$j('.navbar-collapse a').removeAttr('tabindex');
 	}
 	
 	if ($j(window).width() <= mobileNavMaxBreak) {
-		$j('body.page').removeClass('desktopNav');
-		$j('body.page').addClass('burgerNav');
+		$j('body').removeClass('desktopNav');
+		$j('body').addClass('burgerNav');
 		$j('header .navbar-toggle').removeAttr('tabindex').attr('aria-label','click to expand navigation');
 		$j('.navbar-header .sr-only').text('menu is collapsed');
 		$j('.navbar-collapse a').attr('tabindex','-1');
 	}
 	
-	if($j('body.page').hasClass('isMobile desktopNav')){
-		$j('body.page').addClass('burgerNav').removeClass('desktopNav');
+	if($j('body').hasClass('isMobile desktopNav')){
+		$j('body').addClass('burgerNav').removeClass('desktopNav');
 	}
 }
 
