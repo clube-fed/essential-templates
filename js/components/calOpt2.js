3576bf7f34c2a4215ee1102b4b13b7165686ae8c
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-02-20 11:18:45 -0500

    Update calOpt2.js

--- js/components/calOpt2.js
+++ js/components/calOpt2.js
@@ -1,8 +1,11 @@
 //calendar option 2
-$j('.calendar.opt2 [id$="EventsWrapper"] > div').each(function () {
-    $j('.event-image', this).css('background-image', "url(" + $j('.event-image img', this).attr("src") + ")");
+$j('.calendar.opt2 [id$="EventsWrapper"] > div').each(function(){
+    $j(this).contents().unwrap();
+});  
+$j('.calendar.opt2 [id$="EventsWrapper"] > a').each(function(){
+    $j('.event-image', this).css('background-image', "url("+ $j('.event-image img', this).attr("src") +")");
     //var eventTitle = $j('.event-title',this).text();
     //$j( '.event-image img:first-child', this ).attr('alt',eventTitle);
-    $j('.event-image img', this).attr('alt', '');
-    $j('.event-image img + img', this).attr('aria-hidden', 'true').attr('alt', '');
+    $j( '.event-image img', this ).attr('alt','');
+    $j( '.event-image img + img', this ).attr('aria-hidden','true').attr('alt','');
 });
