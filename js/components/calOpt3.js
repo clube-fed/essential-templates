afbef5c348526722e41865f2ef051c33d6ec7834
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-03-27 16:10:38 -0400

    Update calOpt3.js

--- js/components/calOpt3.js
+++ js/components/calOpt3.js
@@ -1,9 +1,11 @@
 //calendar option 3
-$j('.calendar.opt3 [id$="EventsWrapper"] > div').each(function () {
+$j('.calendar.opt3 [id$="EventsWrapper"] > div').each(function(){
+    $j(this).contents().unwrap();
+});
+$j('.calendar.opt3 [id$="EventsWrapper"] > a').each(function () {
     $j('.event-image', this).css('background-image', "url(" + $j('.event-image img', this).attr("src") + ")");
     //var eventTitle = $j('.event-title',this).text();
     //$j( '.event-image img:first-child', this ).attr('alt',eventTitle);
     $j('.event-image img', this).attr('alt', '');
     $j('.event-image img + img', this).attr('aria-hidden', 'true').attr('alt', '');
-    $j(this).contents().unwrap();
 });
