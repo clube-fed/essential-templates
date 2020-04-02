41e8317f3fbf0b49cba2d5d4268d0452f76fbf6f
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-04-02 14:35:05 -0400

    Update subnav.js

--- js/components/subnav.js
+++ js/components/subnav.js
@@ -1,6 +1,6 @@
 //subnav
 $j('.content .sub-nav').each(function () {
     if (!$j.trim($j('ul.ulMenu', this).html()).length) {
-        $j(this).parent().parent().removeClass('d-lg-block');
+        $j(this).removeClass('d-lg-block');
     }
 });
