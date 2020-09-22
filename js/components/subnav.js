2563aa71a948d8b1cf546c5d4a29198c00fc0607
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-09-22 16:08:20 -0400

    Update subnav.js

--- js/components/subnav.js
+++ js/components/subnav.js
@@ -1,5 +1,12 @@
-//subnav
-$j('.content .sub-nav').each(function () {
+//subnav v1
+$j('.content .col-lg-3').each(function () {
+    if (!$j.trim($j('.subnav ul.ulMenu', this).html()).length) {
+        $j(this).removeClass('d-lg-block');
+    }
+});
+
+//subnav v2
+$j('.content .sub-nav.opt2').each(function () {
     if (!$j.trim($j('ul.ulMenu', this).html()).length) {
         $j(this).removeClass('d-lg-block');
     }
