e8f787a6bf3824cd7b8dcd5aab2089b3c92ef93b
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-10-02 11:29:09 -0400

    Update globalScripts.js

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -861,12 +861,21 @@ $j( '.content .tsContent .mpContent img:not([alt])' ).each( function(){
 	$j(this).attr('alt','Image for Tab Strip Content');
  });
 
-/******************* 
-add class when subnav is empty 
+/*******************
+subnav v1
 ******************/
-$j( '.content .sub-nav' ).each( function(){
-if(!$j.trim($j('ul.ulMenu',this).html()).length) { 
-$j(this).removeClass('d-lg-block');
+$j('.content .col-lg-3').each(function () {
+    if (!$j.trim($j('.sub-nav ul.ulMenu', this).html()).length) {
+        $j(this).removeClass('d-lg-block');
+    }
+});
+
+/*******************
+subnav v2
+******************/
+$j('.content .sub-nav.opt2').each(function () {
+    if (!$j.trim($j('ul.ulMenu', this).html()).length) {
+        $j(this).removeClass('d-lg-block');
     }
 });
 
