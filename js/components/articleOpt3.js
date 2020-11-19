d6655fe8fc551737559d4d2760cdef2fd919ff46
ce-joe <42188640+ce-joe@users.noreply.github.com>
2020-11-19 13:55:04 -0500

    Update and rename articleOpt4.js to articleOpt3.js

--- js/components/articleOpt3.js
+++ js/components/articleOpt3.js
@@ -1,5 +1,5 @@
-//article option 4
-$j('.articles.opt4 .newsItem').each(function () {
+//article option 3
+$j('.articles.opt3 .newsItem').each(function () {
     var articleSubject = $j('.article-subject', this).text();
     $j('.article-image', this).css('background-image', "url(" + $j('.article-image img', this).first().attr("src") + ")");
     $j('.article-image img', this).first().attr('alt', articleSubject);
