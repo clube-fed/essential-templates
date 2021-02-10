6cdda86f6d488060b07bdc4c3afc2fd35251c219
ce-joe <42188640+ce-joe@users.noreply.github.com>
2021-02-10 13:05:22 -0500

    Update globalScripts.js

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -213,11 +213,11 @@ $j('header.opt2').each(function () {
 $j('header.opt4').each(function(){
     var last_nav_item_text = $j.trim($j('.navbar-nav > li:last-child > .nav-link > .rootLevelSpan',this).text());
     $j('.navbar-nav > li:last-child > .nav-link', this).clone().removeClass('.nav-link').addClass('log-link').appendTo('.navbar');    
-    if (last_nav_item_text == 'Login') {
-        $j('a.log-link', this).text('Login');
+    if (last_nav_item_text == 'Logout') {
+        $j('a.log-link', this).text('Logout');
     }
     else {
-        $j('a.log-link', this).text('Logout');
+        $j('a.log-link', this).text('Login');
     }
     $j('a.log-link', this).wrap( "<div class='nav-log-wrap'></div>" );
     $j('a.log-link', this).prepend( "<span class='nc-icon-outline ui-1_lock'></span>" );
