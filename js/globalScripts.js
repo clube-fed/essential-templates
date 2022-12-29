81691dcbfdd83e1f7ef3494bfee548dd81956297
ce-joe <42188640+ce-joe@users.noreply.github.com>
2022-12-29 14:35:07 -0500

    Update globalScripts.js

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -396,6 +396,22 @@ $j('.calendar.opt3 [id$="EventsWrapper"] > a').each(function () {
 $j('.calendar.opt4 > .mpContent:first-child').each(function () {
     $j(this).css('background-image', "url(" + $j('img', this).attr("src") + ")");
 });
+
+//articles option 3
+$j('.articles.opt3 .newsItem').each(function () {
+    var articleImageLinkURL = $j('.article-image-link-url', this).text();
+    var articleSubject = $j('.article-subject', this).text();
+    $j('.article-image', this).css('background-image', "url(" + $j('.article-image img', this).first().attr("src") + ")");
+    $j('.article-image img', this).first().attr('alt', articleSubject);
+    $j('.article-image img + img', this).attr('aria-hidden', 'true').attr('alt', '');
+    if($j.trim($j('.article-image-link-url',this).html()).length) { 
+        //$j('.article-wrap', this).addClass('empty-banner');
+        $j('.article-wrap', this).attr( "href", articleImageLinkURL);
+        $j('.article-wrap:not([href*="ceclients"]):not([href*="westwoodcountryclub.com"])', this).attr( "target", '_blank');
+        $j('.article-wrap[href*=".pdf"]', this).attr( "target", '_blank');
+    }
+});
+
 $j('.sitewrap.login #login-bg, .sitewrap.login .login-right').each(function () {
     if ($j.trim($j('.login-bg-img .mpContent', this).html()).length) {
         $j(this).css('background-image', "url(" + $j(this).find('.login-bg-img .mpContent').children('img').attr("src") + ")");
