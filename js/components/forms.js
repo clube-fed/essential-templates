f117764a42471366e9917ee35cf8fe58d5be1854
ce-joe <42188640+ce-joe@users.noreply.github.com>
2023-08-16 11:11:03 -0400

    Update forms.js
    Added script to fix colspan issue on section headings

--- js/components/forms.js
+++ js/components/forms.js
@@ -1,4 +1,6 @@
 //formbase edits
-$j('.formBaseFormWrapper > div:eq(1)').addClass('formBtn');
-$j('.formBaseFormWrapper > div:eq(1)').addClass('clearFix');
-$j('.formBaseFormWrapper > div:eq(1) > div:eq(1)').addClass('secureLink');
+//$j('.formBaseFormWrapper > div:eq(1)').addClass('formBtn');
+//$j('.formBaseFormWrapper > div:eq(1)').addClass('clearFix');
+//$j('.formBaseFormWrapper > div:eq(1) > div:eq(1)').addClass('secureLink');
+
+$j('.formBaseForm .formSection tr.rosterletteroff > td[colspan]').removeAttr('colspan');
